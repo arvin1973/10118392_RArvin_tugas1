f75492e2cad051a29ce61157a5cef7e664784687
arvin1973 <62689804+arvin1973@users.noreply.github.com>
2020-03-27 11:16:48 +0700

    Add files via upload

---
+++ kelasutama.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Arvin Rizky
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        //System.out.println("Hallo");
+        //hidupan objek atau intiansiasi
+        //frameutama futama = new frameutama();
+        //futama.tampil("Program Pertama GUI");
+        
+        Futama utama= new Futama();
+        utama.setTitle("pemograman");
+        utama.setVisible(true);
+    }
+}
